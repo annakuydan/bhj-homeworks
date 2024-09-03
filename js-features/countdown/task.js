5f491491ad2b1dc7a62504a8fe0dd9db30ae5f29
Anna <hanni.k@mail.ru>
2024-09-03 18:00:57 +0300

    all tasks have been completed

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -2,14 +2,14 @@
 
 let startTimer = 59; 
 let element = document.getElementById('timer');
-
+let counter = setInterval(changeTimer, 100);
 function changeTimer() {
 	if (startTimer === 0) {
-		clearInterval(interval);
+		clearInterval(counter);
 		alert('Вы победили в конкурсе!');
 	}
 	element.textContent = startTimer;
 	startTimer --;
 }
 
-setInterval(startTimer, 100);
+
